7115aba60ca5a9fed95544da3f768e112c358c7a
vinem <vinem305@mail.ru>
2026-08-28 00:30:32 +0700

    fix task-manager

--- task-manager/backend/src/main/java/cors/CreateCors.java
+++ task-manager/backend/src/main/java/cors/CreateCors.java
@@ -4,7 +4,7 @@ import com.sun.net.httpserver.HttpExchange;
 
 public class CreateCors {
     public void cors(HttpExchange exchange) {
-        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "http://localhost:5050");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "https://portfollio-gab.ru/");
         exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");
         exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
